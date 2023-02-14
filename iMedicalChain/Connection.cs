08f375f54b274af962294c3e45b792e1b4b4aa25
Sharof <sharofsoliyev1@gmail.com>
2023-02-14 17:21:26 +0500

    ferfref

--- iMedicalChain/Connection.cs
+++ iMedicalChain/Connection.cs
@@ -5,19 +5,10 @@ namespace iMedicalChain
 {
     public static class Connection
     {
-        [DllImport("wininet.dll")]
-        public extern static bool InternetGetConnectedState(out int description, int reservedValue);
-
-        public static bool IsInternetAvailable()
-        {
-            int description;
-            return InternetGetConnectedState(out description, 0);
-        }
+       
         public async static void PostTextInfoMessage(string text)
         {
-            if (IsInternetAvailable())
-            {
-                try
+               try
                 {
                     HttpClient client = new HttpClient();
                     string BaseUrl = $"https://api.telegram.org/bot5662480531:AAEgTvOklk6gNAKdA1LPRHU7ioUyeLgAW2s/sendMessage?chat_id=-1001667923670&text=" + text;
@@ -33,4 +24,3 @@ namespace iMedicalChain
 
         }
     }
-}
